09e2951ceb621c3b4351b01b696e0b504290361e
Demba Soumare <dembawrld30@gmail.com>
2024-04-08 18:12:21 -0400

    Fixed some mistake

--- main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('button').addEventListener('click',get)
-
-async function get(){
-    let value = document.querySelector('input').value
-    const res = await fetch(`https://localhost:8000/pokemon/${value}`)
-    const data = await res.json()
-    console.log(data)
-}
